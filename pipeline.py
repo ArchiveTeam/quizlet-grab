34d192bf7be7051edd9c220e4ebac2ecf4230e15
Adin Biederman <adinbied@gmail.com>
2018-09-22 13:34:46 -0700

    Update version to 20180922.01

--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20180827.01'
+VERSION = '20180922.01'
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'quizlet'
 TRACKER_HOST = 'tracker.archiveteam.org'
